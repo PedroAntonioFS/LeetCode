2dd0bc17c588d51aabe72df4abb99406256cde36
Pedro Antonio <p.antonio.f.s@gmail.com>
2021-05-06 17:55:34 -0300

    Resposta O(n)
    Tempo: O(n). Memória: O(n).

--- 1-two-sum.py
+++ 1-two-sum.py
@@ -4,26 +4,13 @@ class Solution:
         begin = 0
         end = len(nums)-1
 
-        nums_sorted = sorted(nums)
-        while True:
-            if nums_sorted[begin] + nums_sorted[end] == target:
-                break
-            elif nums_sorted[begin] + nums_sorted[end] > target:
-                end -= 1
-            else:
-                begin += 1
-
-        sol = []
-        index = 0
-        while len(sol) < 2:
-            if (nums[index] == nums_sorted[begin]) and (nums[index] != nums_sorted[end]):
-                sol.append(index)
-            elif nums[index] == nums_sorted[end]:
-                sol.append(index)
-            index += 1
-        
-        return sol
+        hash_nums = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if hash_nums.get(complement) != None:
+                return [hash_nums.get(complement), i]
+            hash_nums[nums[i]] = i
 
 instance = Solution
 
-print(instance.twoSum(Solution,nums=[3,2,3], target=6))
+print(instance.twoSum(Solution,nums=[2,7,11,15], target=9))
